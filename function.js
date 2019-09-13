5b39a1b21215741de45c8534625e2a2f3fc781ee
michael <mamikonyan.michael@gmail.com>
2019-09-13 18:32:20 +0400

    100 porci hamar

--- function.js
+++ function.js
@@ -28,7 +28,7 @@ function kungrument(kung_n,kung_t,kung_k,r_i_qanak) {
     return r_i;
 }
 
-function puason(lyamda,kung_n,kung_t,kung_k,r_i,l) {
+function puason(lyamda,r_i,l) {
     let j=-1;
     let s=1;
     // let  r_i = kungrument(kung_n,kung_t,kung_k,1000);
@@ -47,8 +47,40 @@ function workers(m) {
         work[i].t_i = 0;
         work[i].finish_time = 0
         work[i].free = true;
+        work[i].serve_all_time = 0;
     }
     return work;
 
 
+}
+
+function random(T,lyamda_mutq,lyamda_serve,a,b,kung_n,kung_t,kung_k) {
+	// kung-ery kungurenti popoxakannern en
+	let array = new Array();
+	let i = 1;
+	array[0] = new Object();
+	array[0].t_i = 0;
+	let t_i = 0;
+
+	let  r_i = new Array();
+	r_i = kungrument(kung_n,kung_t,kung_k,1000);
+
+	do{
+		array[i] = new Object();
+
+		array[i].tao_i = puason(lyamda_mutq,r_i,i);
+		// array[i].tao_i = 1;
+		array[i].t_i = array[i-1].t_i + array[i].tao_i;
+		array[i].serve = puason(lyamda_serve,r_i,i+100);
+		// array[i].serve = 10;
+		array[i].wait_time = a+(b-a)*r_i[i];
+		++i;
+	} while (array[i-1].t_i < T);
+
+	array.pop(); // jamic ushacoxin jnjuma
+	array.shift(); // arajiny vor menak 0 a jnjuma
+
+	// let ii = array.indexOf(Math.min(...array));
+	// console.log(ii);
+	return array;
 }
